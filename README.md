15218e2394a08515a1bfc0147042a14e4af92d0e
Ravindran A <ravindran.abiraman@gmail.com>
2021-07-20 22:46:21 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# FoodyCookBook
+FoodyCookBook - Native iOS Application
+
+Supported from iOS 13.0 and above
+
+Works in iPhone(Portrait)
+
+iPad Yet to be supported
+
+Main Project Target:
+
+FoodyCookBook
+
+Supporting Third Party Libraries Used:
+
+SwiftLint - Linter for Swift
---
+++ source/FoodyCookBook/FoodyCookBook/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FoodyCookBook
+//
+//  Created by Ravindran on 20/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
